4d9224126b0ee5c63378c69c53f3bd188361627e
Denis Steinhorst <denis@steinhorst.dev>
2025-09-15 07:42:51 +0200

    fix: remove Usercentrics and Hotjar scripts after trial

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -144,31 +144,6 @@ export default defineNuxtConfig({
         { rel: 'canonical', href: 'https://www.steinhorst.dev' },
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       ],
-      script:
-        process.env.NODE_ENV === 'production'
-          ? [
-              {
-                id: 'usercentrics-cmp',
-                src: 'https://web.cmp.usercentrics.eu/ui/loader.js',
-                'data-settings-id': 'h94zxBp1utgWJs',
-                async: true,
-              },
-              {
-                type: 'text/plain',
-                'data-usercentrics': 'Hotjar',
-                innerHTML: `
-            (function(h,o,t,j,a,r){
-              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:6513298,hjsv:6};
-              a=o.getElementsByTagName('head')[0];
-              r=o.createElement('script');r.async=1;
-              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-              a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-          `,
-              },
-            ]
-          : [],
     },
   },
 })
